30f1e7ee65d8ad4663e54b56ba9f18129100f8ef
jwoz <p45052000@yahoo.com>
2020-05-18 13:30:55 -0400

    debugging

--- py/learn.py
+++ py/learn.py
@@ -7,7 +7,7 @@ from j2learn.layer.cnn import CNN
 from j2learn.layer.dense import Dense
 from j2learn.layer.image import Image
 
-mndata = MNISTData()
+mndata = MNISTData(path='C:/Users/juergen/Dropbox/data/mnist')
 
 images, labels = mndata.training()
 
@@ -19,13 +19,15 @@ category = Category([i for i in range(10)], dense)
 
 print(category.node(1).value())
 
-r = random.randint(0, 500)
+# r = random.randint(0, 500)
+r=49 # a nice number three
 image_layer.set_image_data_and_label(images[r], labels[r])
+print(image_layer.display())
 
-category.node(1).value()
 
-# first_cnn.build()
-print(first_cnn.node(16, 5).value())
+first_cnn.build()
+print(first_cnn.node(155).value())
 
 print(first_cnn.node(16, 5)._weights)
 print(first_cnn.node(16, 5)._underlying_nodes)
+print(category.node(1).value())
