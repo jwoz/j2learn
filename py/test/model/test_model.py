f2e42f627ee361992a09e2d94256dafcc81d614e
jwoz <p45052000@yahoo.com>
2020-05-22 07:41:12 -0400

    derivatives

--- py/test/model/test_model.py
+++ py/test/model/test_model.py
@@ -10,45 +10,47 @@ from j2learn.model.model import Model
 
 
 class TestModel(TestCase):
+    def _rum_derivatives_test(self, model, expected_no_weights=None, expected_no_partials=None):
+        model.compile(build=True)
+        weight_count = model.weight_count()
+        if expected_no_weights is not None:
+            self.assertEqual(weight_count, expected_no_weights)
+
+        mod_jacb = list(flatten(model.jacobian()))
+        if expected_no_partials is not None:
+            self.assertEqual(len(mod_jacb), expected_no_partials)
+
+        bumped_derivatives = []
+        for i in range(weight_count):
+            d = finite_difference(model, i, False)
+            bumped_derivatives.append(d)
+        non_zero_bumped_derivatives = [b for b in flatten(bumped_derivatives) if b != 0]
+        for m, b in zip(mod_jacb, non_zero_bumped_derivatives):
+            self.assertAlmostEqual(m, b, 4)
+        print(mod_jacb)
+
     def test_jacobian_small(self):
         image = [random.randint(0, 255) for _ in range(4)]
         small_image = Image(image_data=image)
         dense = Dense(reLU(), (2, 1))
         cnn = CNN(reLU(), (1, 2), (0, 0))
         model = Model(layers=[small_image, cnn, dense])
-        model.compile(build=True)
-        weight_count = model.weight_count()
-        self.assertEqual(weight_count, 14)
+        self._rum_derivatives_test(
+            model,
+            14,
+            20  # + 8 zerp derivatives
+        )
 
-        mod_jacb = list(flatten(model.jacobian()))
-        self.assertEqual(len(mod_jacb), 20) # + 8 zero derivatives
-
-        bumped_derivatives = []
-        for i in range(weight_count):
-            d = finite_difference(model, i, False)
-            bumped_derivatives.append(d)
-        non_zero_bumped_derivatives = [b for b in flatten(bumped_derivatives) if b !=0]
-        for m, b in zip(mod_jacb, non_zero_bumped_derivatives):
-            self.assertAlmostEqual(m, b, 4)
-        print(mod_jacb)
-
-    def test_jacobian_medium(self):
-        image = [random.randint(0, 255) for _ in range(16)]
-        image = Image(image_data=image)
-        dense = Dense(reLU(), (2, 1))
-        cnn_a = CNN(reLU(), (1, 2), (0, 0))
-        cnn_b = CNN(reLU(), (3, 3), (0, 0))
-        model = Model(layers=[image, cnn_a, cnn_b, dense])
-        model.compile(build=True)
-        weight_count = model.weight_count()
-
-        mod_jacb = list(flatten(model.jacobian()))
-
-        bumped_derivatives = []
-        for i in range(weight_count):
-            d = finite_difference(model, i, False)
-            bumped_derivatives.append(d)
-        non_zero_bumped_derivatives = [b for b in flatten(bumped_derivatives) if b !=0]
-        for m, b in zip(mod_jacb, non_zero_bumped_derivatives):
-            self.assertAlmostEqual(m, b, 4)
-        print(mod_jacb)
+    def test_jacobian_cnn(self):
+        image = [random.randint(0, 255) for _ in range(2)]
+        image = Image(image_data=image, shape=(1, 2))
+        cnn_a = CNN(reLU(), (1, 1), (0, 0))
+        cnn_b = CNN(reLU(), (1, 1), (0, 0))
+        model = Model(layers=[
+            image,
+            cnn_a,
+            cnn_b,
+        ])
+        self._rum_derivatives_test(
+            model
+        )
